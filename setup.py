c42a4bc5e1590fdf473f3afb3d965f3d48570862
Zehina <zehinadev@gmail.com>
2021-09-26 22:43:04 +0100

    format: update setup file

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='Webtoon_Downloader',
-    version='0.3.0',
-    description='Webtoons Scraper for downloading chapters of any series wanted.',
+    version='0.5.1',
+    description='Webtoons Scraper for downloading chapters of any series hosted on the webtoons website.',
     author='Ali Taibi',
     author_email='zehinadev@gmail.com',
     url='https://github.com/Zehina/Webtoon-Downloader',
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(where='src'), 
     package_dir={'': 'src'},
     python_requires='>=3.7, <4',
-    install_requires=['requests>=2.26.0', 'beautifulsoup4>=4.10.0', 'rich>=10.10.0'],
+    install_requires=['requests>=2.26.0', 'beautifulsoup4>=4.10.0', 'rich>=10.10.0', 'Pillow>=8.3.1', 'lxml>=4.6.3'],
     project_urls={
         'Bug Reports': 'https://github.com/Zehina/Webtoon-Downloader/issues',
         'Source': 'https://github.com/Zehina/Webtoon-Downloader/',
