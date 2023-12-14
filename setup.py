df6d232893e5f541e1df4bffe9af82d3d6e7771d
Olaf Mandel <olaf@mandel.name>
2023-12-14 21:42:32 +0100

    Add console_script `webtoon-downloader`
    Declare a console_script to be installed that executes the main
    function. This allows a user to just install this module and then
    execute the functionality from their path without having to specify the
    interpreter or the module-name every time.

--- setup.py
+++ setup.py
@@ -34,6 +34,11 @@ setup(
     },
     python_requires='>=3.7, <4',
     install_requires=['requests>=2.26.0', 'beautifulsoup4>=4.10.0', 'rich>=10.10.0', 'Pillow>=8.3.1', 'lxml>=4.6.3'],
+    entry_points={
+        'console_scripts': [
+            'webtoon-downloader = Webtoon_Downloader.webtoon_downloader:main',
+        ],
+    },
     project_urls={
         'Bug Reports': 'https://github.com/Zehina/Webtoon-Downloader/issues',
         'Source': 'https://github.com/Zehina/Webtoon-Downloader/',
